f8a039015248bf355ea6718aa56432bfb21045aa
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-02-21 08:59:05 +0530

    AspNetIdentityDbContext

--- API/Controllers/CoffeeShopController.cs
+++ API/Controllers/CoffeeShopController.cs
@@ -1,27 +1,26 @@
 ﻿using API.Services;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	[Authorize]
-	public class CoffeeShopController : ControllerBase
-	{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CoffeeShopController : ControllerBase
+    {
         private readonly ICoffeeShopService coffeeShopService;
 
-		public CoffeeShopController(ICoffeeShopService coffeeShopService)
-		{
-			this.coffeeShopService = coffeeShopService;
-		}
+        public CoffeeShopController(ICoffeeShopService coffeeShopService)
+        {
+            this.coffeeShopService = coffeeShopService;
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> List()
-		{
-			var coffeeShops = await coffeeShopService.List();
-			return Ok(coffeeShops);
-		}
-	}
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var coffeeShops = await coffeeShopService.List();
+            return Ok(coffeeShops);
+        }
+    }
 }
--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -1,19 +1,15 @@
 ﻿using DataAccess.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Data
 {
-	public class ApplicationDbContext : DbContext
-	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
-		{ }
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
-		public DbSet<CoffeeShop> CoffeeShops { get; set; }
-	}
+        public DbSet<CoffeeShop> CoffeeShops { get; set; }
+    }
 }
